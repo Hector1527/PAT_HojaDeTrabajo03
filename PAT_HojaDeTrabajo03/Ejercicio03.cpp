06296d67ffef653c3b3a325bcb1ffba455530f7a
Hector1527 <hectorperezgonzalez09@gmail.com>
2023-11-25 18:31:05 -0600

    Ejercicio 3

--- PAT_HojaDeTrabajo03/Ejercicio03.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio03.cpp
@@ -11,13 +11,34 @@ Node<int>* Ejercicio03::copyList(Node<int>* head)
 
 	Node<int>* result = new Node<int>;
 	result->value = head->value;
+	result->next = nullptr;
+	result->random = nullptr;
 	copianodo[head] = result;
 
 	Node<int>* original = head->next;
 	Node<int>* copia = result;
 
-	while (original)
+	while (original!= nullptr)
 	{
+		copia->next = new Node<int>;
+		copia->next->value = original->value;
+		copianodo[original] = copia->next;
+		copia->next->next = nullptr;
 
+		original = original->next;
+		copia = copia->next;
 	}
+
+	original = head;
+	copia = result;
+
+	while (original!= nullptr)
+	{
+		if (original->random)
+			copia->random = copianodo[original->random];
+
+		original = original->next;
+		copia = copia->next;
+	}
+	return result;
 }
